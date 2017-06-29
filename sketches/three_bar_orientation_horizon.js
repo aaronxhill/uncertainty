3b602b52d6e8b40cfb07567023aa3f6bed9bbf46
Steven Hubbard <hubbs654@newschool.edu>
2017-06-29 12:24:09 -0400

    Three bars and orientation
    For the three bars and orientation.  I just changed the orientation of the bars so the mid line was the reference point instead of the bottom line.  Maybe?

---
+++ sketches/three_bar_orientation_horizon.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(320, 320);
+}
+
+function draw() {
+
+  line(40, 160, 280, 160);
+
+  fill(200);
+  rect(69, 113, 40, 94);
+
+  fill(130);
+  rect(140, 40, 40, 241);
+
+  fill(90);
+  rect(210, 77, 40, 166);
+}
