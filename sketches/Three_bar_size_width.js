c491d242a64a4bea17a556ba48ced94dfe888d56
Steven Hubbard <hubbs654@newschool.edu>
2017-06-29 12:25:32 -0400

    three bars for size with varying widths
    changed the widths of the three bars so it changed the size.

---
+++ sketches/Three_bar_size_width.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(320, 320);
+}
+
+function draw() {
+
+  line(40, 280, 280, 280);
+
+  fill(200);
+  rect(49, 186, 80, 94);
+
+  fill(130);
+  rect(160, 39, 20, 241);
+
+  fill(90);
+  rect(210, 114, 40, 166);
+}
