e5f1df37b2240b1a2735c6c4913b9d8bc94ef7d9
Aaron Hill <hillar@newschool.edu>
2017-07-03 14:08:38 +0000

    more cells completed

---
+++ cells/_combine.js
@@ -0,0 +1,10 @@
+var xfs = require('fs');
+
+xfs.truncate('/home/ubuntu/workspace/uncertainty/public/sketch.js', 0, function() {
+    for (var i = 0; i < files.length; i++) {
+        xfs.appendFileSync('/home/ubuntu/workspace/uncertainty/public/sketch.js', xfs.readFileSync(files[i]));
+    }
+});
+
+var files = ['po.js', 'ptb.js', 'ps.js', 'lx.js', 'lo.js', 'ls.js', 'lt.js', 
+'cx.js', 'cz.js', 'co.js', 'gx.js', 'gs.js', 'gc.js'];
--- cells/combine.js
+++
@@ -1,7 +0,0 @@
-var xfs = require('fs');
-
-var files = ['po.js', 'ptb.js', 'ps.js', 'lx.js', 'lo.js', 'cx.js', 'cz.js', 'co.js', 'gx.js', 'gs.js'];
-
-for (var i=0; i < files.length; i++) {
-    xfs.appendFileSync('assembled.js', xfs.readFileSync(files[i]));
-}
