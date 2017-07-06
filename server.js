4def18e1df66983231d203ac3737da9a5c280dfc
Aaron Hill <aaron.hill@me.com>
2017-07-06 12:56:50 -0400

    update divs for bootstrap

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.listen(process.env.PORT, function () {
+app.listen(process.env.PORT || 8080, function () {
   console.log('Example app listening on port ' + process.env.PORT);
 })
