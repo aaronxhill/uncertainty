a31eb4857e93d4eca9068adcfb61462a037ab63c
Aaron Hill <aaron.hill@me.com>
2018-07-19 15:33:47 -0400

    new from aws

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use(express.static('public'));
-
-app.listen(process.env.PORT || 8080, function () {
-  console.log('Example app listening on port ' + process.env.PORT);
-})
